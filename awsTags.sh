4fb6b2783090824aa18d38718fc2ef3700b85619
Max Fortun <Max.Fortun@wsj.com>
2024-05-13 13:29:16 -0400

    updated

--- awsTags.sh
+++ awsTags.sh
@@ -1,10 +1,47 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-filters=()
-[ -z "$2" ] || filters+=( --tag-filters )
-while [ -n "$2" ]; do
-	filters+=( "Key=$1,Values=$2" )
-	shift 2
+include=()
+exclude=()
+
+mode=
+while [ -n "$1" ]; do
+	if [[ "$1" =~ [-+] ]]; then
+		mode=$1
+		shift
+		continue
+	fi
+
+	case "$mode" in
+		+)
+			if [ -z "$2" ]; then
+				echo "Invalid number of parameters for inclusions. Required 2. Name and value." >&2
+				exit 1
+			fi
+		
+			include+=( "Key=$1,Values=$2" )
+			shift 2
+		;;
+		-)
+			if [ -z "$2" ]; then
+				echo "Invalid number of parameters for inclusions. Required 2. Name and value." >&2
+				exit 1
+			fi
+
+			exclude+=( 'ResourceTagMappingList[?Tags[?Key == `'$1'` && Value != `'$2'`]]' )
+			shift 2
+		;;
+		*)
+			echo "Unknow mode: $mode" >&2
+			echo "Valid modes:" >&2
+			echo "+: Include" >&2
+			echo "-: Exclude" >&2
+			exit 1
+		;;
+	esac
+	
 done
 
-aws --region $AWS_REGION --no-cli-pager resourcegroupstaggingapi get-resources "${filters[@]}"
+[ -z "${include[@]}" ] || include=( --tag-filters "${include[@]}" )
+[ -z "${exclude[@]}" ] || exclude=( --query "${exclude[@]}" )
+
+aws --region $AWS_REGION --no-cli-pager resourcegroupstaggingapi get-resources "${include[@]}" "${exclude[@]}"
